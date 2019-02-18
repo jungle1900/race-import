9a6f80742cb0d6dba6f74d902ded1de3e0f942be
Greene <jungle1900@gmail.com>
2019-02-18 19:31:55 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Race from 'race-pixi'
+
+const race = new Race(document.getElementById('game'), 100)
+race.run()
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: "source-map",
+  devServer: {
+    overlay: true,
+    compress: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin('dist'),
+    new CopyWebpackPlugin([
+      { from: 'node_modules/race-pixi/assets', to: '' },
+    ], {
+      ignore: ['.DS_Store']
+    }),
+  ]
+}
